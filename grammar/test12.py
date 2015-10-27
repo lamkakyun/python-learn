5f76088ba18fdad600b4e548783fe803cc1fba4c
lamkakyun <lamkakyun@gmail.com>
2015-10-27 18:18:23 +0800

    add test

--- grammar/test12.py
+++ grammar/test12.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-#
+#  进程和异常
+import multiprocessing
+import time
+import signal
+
+p = multiprocessing.Process(target=time.sleep, args=(1000,))
+print(p, p.is_alive())
+p.start()
+print(p, p.is_alive())
+p.terminate()
+time.sleep(0.1)
+print(p, p.is_alive())
+print(p.exitcode == -signal.SIGTERM)
--- test/settings/README.md
+++ test/settings/README.md
@@ -1,4 +1,3 @@
 software design
 
 1. 默认有配置文件,可以设置
-2.
